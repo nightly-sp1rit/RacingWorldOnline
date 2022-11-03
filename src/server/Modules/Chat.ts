6d9231a70e9f5c2b23dde3c03f09bd0df4f78c83
Nightly <105944807+nightly-spirit@users.noreply.github.com>
2022-11-03 15:44:26 +0100

    Added .gitignore, Edited Chat.ts

--- src/server/Modules/Chat.ts
+++ src/server/Modules/Chat.ts
@@ -6,6 +6,8 @@ const Cooldowns: PunishmentArrayType[] = [];
 const ReplicatedStorage = game.GetService("ReplicatedStorage");
 const ServerClientEvent = ReplicatedStorage.WaitForChild("ServerClientEvent") as RemoteEvent;
 
+const TextService = game.GetService("TextService");
+
 function IsPlayerMuted(Player: Player): boolean {
     for (let i = math.round(Muted.size() / 2); i > 0; i--) {
         if (Muted[i][0] !== Player) {
@@ -48,7 +50,21 @@ function IsPlayerCooldowned(Player: Player): boolean {
 
 export function HandleChatRequest(Player: Player, Message: string) {
     if (!IsPlayerMuted(Player) && !IsPlayerCooldowned(Player)) {
-        // TODO
+        // Check if Message is valid to send in chat
+
+        if (Message.size() === 0 || Message.size() > 96) { return 1; } // TODO Send a ServerClient request to display an error
+
+        // Get the Filter Digest
+
+        const FilterInstance: TextFilterResult = TextService.FilterStringAsync(Message, Player.UserId, Enum.TextFilterContext.PublicChat);
+        const FilterResult = FilterInstance.GetNonChatStringForBroadcastAsync();
+
+        ServerClientEvent.FireAllClients("NewMessage", {
+            Origin: Player,
+            Message: Message
+            
+            // Add the rest of the Object elements.
+        });
     } else {
         Player.Kick("You're not allowed to exploit!");
 
