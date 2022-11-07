c5d029fd2644b2c6b48ee77347fd692b2e4e526a
Nightly <114367743+nightly-sp1rit@users.noreply.github.com>
2022-11-07 21:00:07 +0100

    Delete out directory

--- out/server/Backend.server.lua
+++
@@ -1,4 +0,0 @@
--- Compiled with roblox-ts v2.0.4
-local Players = game:GetService("Players")
-Players.PlayerAdded:Connect(function(Player) end)
-Players.PlayerRemoving:Connect(function(Player) end)
